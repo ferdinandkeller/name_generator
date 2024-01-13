ffc6a7ae16dca096fad527ee36beeee2548e20df
Ferdinand Keller <ferdinand.keller@edu.devinci.fr>
2024-01-13 17:55:10 +0000

    added code

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="name_generator",
+    version="1.0.0",
+    description="Name Generator for Python",
+    url="https://github.com/ferdinandkeller/name_generator",
+    author="Ferdinand Keller",
+    author_email="ferdinand.keller@proton.me",
+    license="MIT",
+    packages=["name_generator"],
+    install_requires=[],
+    classifiers=[],
+)
