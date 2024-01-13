49b3e7254ab4558b9a2ae28b9c1920ef7b2f7085
Ferdinand Keller <ferdinand.keller@edu.devinci.fr>
2024-01-13 18:01:41 +0000

    expanded setup script

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="name_generator",
     version="1.0.0",
     description="Name Generator for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/ferdinandkeller/name_generator",
     author="Ferdinand Keller",
     author_email="ferdinand.keller@proton.me",
     license="MIT",
     packages=["name_generator"],
     install_requires=[],
-    classifiers=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    keywords="name generator name_generator moby docker docker-names",
 )
